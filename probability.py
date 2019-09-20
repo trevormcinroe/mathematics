c7eeaf3fb9bd0ded2db2adf0dcae88b8a3117c73
trevor_mcinroe <tf.research.main@gmail.com>
2019-09-20 08:41:50 -0500

    Initial commit

---
+++ probability.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+import math
+import numpy as np
+
+
+#######################
+# == Distributions == #
+#######################
+def pdf_normal(x, mu, sigma):
+    """Probability that our sample, x, from our random variable, x, is from ~N(mu, sigma)
+    
+    Args:
+        x:
+        mu:
+        sigma:
+
+    Returns:
+
+    """
+
