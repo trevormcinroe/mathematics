f5097fe2b446e1c909a001e7e60496d52af53b66
trevor_mcinroe <tf.research.main@gmail.com>
2019-09-20 09:04:25 -0500

    completed pdf_norm

--- probability.py
+++ probability.py
@@ -10,14 +10,21 @@ import numpy as np
 # == Distributions == #
 #######################
 def pdf_normal(x, mu, sigma):
-    """Probability that our sample, x, from our random variable, x, is from ~N(mu, sigma)
-    
+    """Probability that our sample, x, from our random variable, x, is from ~N(mu,sigma)
+
     Args:
-        x:
-        mu:
-        sigma:
+        x (numeric): a single value that represents a sample draw from data
+        mu (numeric): a single value that represents the mean of the target distribution
+        sigma (numeric): a single value that represents the standard deviation of the target distribution
 
     Returns:
+        (float) - the probability of x being from ~N(mu,sigma)
 
     """
+
+    return (1 / (math.sqrt(2 * math.pi * sigma**2))) * math.e**( (-(x-mu)**2) / (2 * sigma**2))
+
+
+
+
 
