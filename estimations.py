61fde0da2e28fcf517e85a2ff220913e679c664b
trevor_mcinroe <tf.research.main@gmail.com>
2019-09-24 08:41:28 -0500

    completed pdf_norm

--- estimations.py
+++ estimations.py
@@ -1,3 +1,19 @@
+"""
+
+"""
+
 import math
 import numpy as np
+from probability import pdf_normal, log_likelihood
+
+def mle(x, mu, sigma):
+    """"""
+    # Estimating θ_μ by taking the derivative of the log-likelihood function and
+    # finding the maximum by setting it = 0
+    # δx / δ θ_μ = 0 --> ∑(x) / n
+
+    # Estimating θ_σ by taking the derivative of the log-likelihood function and
+    # finding the maximum by setting it = 0
+    # δx / δ θ_σ = 0 --> ∑(x - μ)^2 / n
+
 
--- probability.py
+++ probability.py
@@ -22,9 +22,25 @@ def pdf_normal(x, mu, sigma):
 
     """
 
-    return (1 / (math.sqrt(2 * math.pi * sigma**2))) * math.e**( (-(x-mu)**2) / (2 * sigma**2))
+    return (1 / (math.sqrt(2 * math.pi * sigma**2))) * math.e**((-(x-mu)**2) / (2 * sigma**2))
 
 
+def log_likelihood(x, mu, sigma):
+    """Log-likelihood that our sample, x, from our random variable, x, is from ~N(mu,sigma)
 
+    Args:
+        x (numeric): a single value that represents a sample draw from data
+        mu (numeric): a single value that represents the mean of the target distribution
+        sigma (numeric): a single value that represents the standard deviation of the target distribution
 
+    Returns:
+        (float) - the probability of x being from ~N(mu,sigma)
+    """
 
+    first_term = -(len(x) / 2) * np.log(2 * math.pi)
+
+    second_term = (len(x) / 2) * np.log(sigma**2)
+
+    summation_term = (1 / (2 * sigma**2)) * np.sum(np.subtract(x, mu)**2)
+
+    return first_term - second_term - summation_term
