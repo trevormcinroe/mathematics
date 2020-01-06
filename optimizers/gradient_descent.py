6d09736831548206c94f00034dd70da5a50b5571
trevor_mcinroe <tf.research.main@gmail.com>
2020-01-06 15:08:28 -0600

    adding optimizers category

---
+++ optimizers/gradient_descent.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+import numpy as np
+
+
+class GradientDescent:
+
+    def __init__(self):
+        pass
+
+    def batch_gd(self):
+        """"""
+        pass
+
+    def sgd(self):
+        """"""
+        pass
+
+    def minibatch_gd(self):
+        """"""
+        pass
